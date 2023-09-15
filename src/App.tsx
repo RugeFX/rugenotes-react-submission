33b0c2f8846e1f1a216476ab126f0e3297243335
rugefx <zackfxg@gmail.com>
2023-09-15 17:37:15 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-6xl text-red-700 font-bold">Vite + React</h1>
+      <div className="">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
